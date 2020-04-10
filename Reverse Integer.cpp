9c08da33ecfb6f81be460eb5e60ad08859304520
Mohak Bahal <46339273+mohakbahal@users.noreply.github.com>
2020-04-10 23:40:43 +0530

    Update Reverse Integer.cpp

--- Reverse Integer.cpp
+++ Reverse Integer.cpp
@@ -5,7 +5,7 @@ public:
         while(x!=0)
         {
             temp=x%10;
-            if (sum > INT_MAX/10 || (sum == INT_MAX / 10 && temp > 7))
+            if (sum > INT_MAX/10 || (sum == INT_MAX / 10 && temp > 7))          
                 return 0;
             if (sum < INT_MIN/10 || (sum == INT_MIN / 10 && temp < -8))
                 return 0;
@@ -15,3 +15,8 @@ public:
         return sum;
     }
 };
+
+//If reverse == Integer.MAX_VALUE/10, then reverse = 2,147,483,640. We know that anything over 2,147,483,647 causes integer overflow, 
+//so pop cannot be greater than 7.
+//Likewise, if reverse == Integer.MIN_VALUE/10, then reverse = -2,147,483,640. We know that anything under -2,147,483,648 causes integer 
+//underflow, so pop cannot be less than -8.
