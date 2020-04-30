e2f6984593a0cc5e18be7090e7990bbecb9c7437
Mohak Bahal <mohakbahal1211@gmail.com>
2020-05-01 02:10:10 +0530

    Solution

---
+++ Remove Duplicates from Sorted List.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head==NULL)
+            return head;
+        ListNode* prev=head;
+        ListNode* curr=head->next;
+        while(curr!=NULL)
+        {
+            if(curr->val!=prev->val)
+            {
+                prev->next=curr;
+                prev=curr;
+            }
+        curr=curr->next;
+        }
+    prev->next=NULL;
+    return head;
+    }
+};
