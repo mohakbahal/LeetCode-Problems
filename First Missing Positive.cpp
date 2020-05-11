5a5725a56e835ebe7ad63108f491af450404afb6
Mohak Bahal <46339273+mohakbahal@users.noreply.github.com>
2020-05-11 22:42:10 +0530

    Create First Missing Positive.cpp

---
+++ First Missing Positive.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int k=1;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]<0)
+                continue;
+            else if(nums[i]==k)
+                k++;
+        }
+        return k;
+    }
+};
